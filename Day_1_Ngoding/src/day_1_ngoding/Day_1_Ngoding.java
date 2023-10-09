cbd9a875b88fe8f1c51f2d8d10decdf0a92c7d01
Madotzz <rahmatrahmadi920@gmail.com>
2023-10-09 15:36:19 +0800

    Day 1

---
+++ Day_1_Ngoding/src/day_1_ngoding/Day_1_Ngoding.java
@@ -0,0 +1,10 @@
+package day_1_ngoding;
+public class Day_1_Ngoding {
+    public static void main(String[] args) {
+        String nama = "Rahmat Rahmadi";
+        int umur = 19;
+        System.out.println("Nama saya " + nama);
+        System.out.println("Umur " + umur + " tahun");
+    }
+    
+}
